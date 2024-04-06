d8321a3f0775cdcdc45c6bc33fa01f0a53676158
mizolf <mislav.cesnik88@gmail.com>
2024-04-06 17:31:45 +0200

    Update login.dart

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ict_aac/screen/home.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,97 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  Widget _emailField() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      height: 60,
+      decoration: BoxDecoration(
+        color: const Color(0xFF6CA8F1),
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6.0,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: const TextField(
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top: 14),
+            prefixIcon: Icon(Icons.email_outlined, color: Colors.white),
+            hintText: 'Unesite email adresu',
+            hintStyle: TextStyle(color: Colors.white38)),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      height: 60,
+      decoration: BoxDecoration(
+        color: const Color(0xFF6CA8F1),
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6.0,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: const TextField(
+        obscureText: true,
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top: 14),
+            prefixIcon: Icon(
+              Icons.password_outlined,
+              color: Colors.white,
+            ),
+            hintText: 'Unesite lozinku',
+            hintStyle: TextStyle(color: Colors.white38)),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ));
+        },
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          backgroundColor: Colors.white,
+          shadowColor: Colors.black,
+        ),
+        child: const Text(
+          'PRIJAVA',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,107 +148,53 @@ class _LoginScreenState extends State<LoginScreen> {
                         'Email',
                         style: TextStyle(
                           color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                       const SizedBox(
                         height: 12,
                       ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF6CA8F1),
-                          borderRadius: BorderRadius.circular(10.0),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 6.0,
-                              offset: Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        child: const TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(top: 14),
-                              prefixIcon: Icon(Icons.email_outlined,
-                                  color: Colors.white),
-                              hintText: 'Unesite email adresu',
-                              hintStyle: TextStyle(color: Colors.white38)),
-                        ),
+                      _emailField(),
+                      const SizedBox(
+                        height: 30,
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
                       const Text(
                         'Lozinka',
                         style: TextStyle(
                           color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                       const SizedBox(
                         height: 12,
                       ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF6CA8F1),
-                          borderRadius: BorderRadius.circular(10.0),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 6.0,
-                              offset: Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        child: const TextField(
-                          obscureText: true,
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(top: 14),
-                              prefixIcon: Icon(
-                                Icons.password_outlined,
-                                color: Colors.white,
-                              ),
-                              hintText: 'Unesite lozinku',
-                              hintStyle: TextStyle(color: Colors.white38)),
-                        ),
-                      ),
+                      _passwordField(),
                       const SizedBox(
                         height: 30,
                       ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(vertical: 25),
-                        width: double.infinity,
-                        height: 100,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            backgroundColor: Colors.white,
-                            shadowColor: Colors.black,
-                          ),
-                          child: const Text(
-                            'PRIJAVA',
-                            style: TextStyle(
-                              color: Color(0xFF527DAA),
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1.5,
-                              fontSize: 18,
-                            ),
+                      _loginButton(),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        '- ILI -',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          'REGISTRIRAJ SE',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
                         ),
                       ),
